8cf7f648131023a54d3e5fc00a04872638ddeeaf
maxsteelcode <max_daniel29@hotmail.com>
2024-01-31 23:48:45 -0300

    Add project files.

---
+++ Basket.Api/Controllers/BasketController.cs
@@ -0,0 +1,41 @@
+﻿using Basket.Api.Entities;
+using Basket.Api.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Basket.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        IBasketRepository _basketRepo;
+
+        public BasketController(IBasketRepository basketRepo)
+        {
+            _basketRepo = basketRepo;
+        }
+
+        [HttpGet("{userName}",Name ="GetBasket")]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _basketRepo.GetBasket(userName);
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart shoppingCart)
+        {
+            return Ok(await _basketRepo.UpdateBasket(shoppingCart));
+        }
+
+        [HttpDelete("{userName}",Name ="DeleteBasket")]
+        public async Task<ActionResult> DeleteBasket(string userName)
+        {
+            await _basketRepo.DeleteBasket(userName);
+            return Ok();
+        }
+
+    }
+}
